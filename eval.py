c80f94896c99fd0393959c80c67d36c24c371a01
Leo Lu <ll865@cornell.edu>
2025-03-05 13:13:26 -0500

    Updated eval code

--- eval.py
+++ eval.py
@@ -1,11 +1,18 @@
 import nn as defs
 import torch
+import helper
 
 # load in trained model
-model = defs.Model()
+model = defs.Model(32, 4)
 model.load_state_dict(torch.load("model.pth"))
+model.eval()
 
 # make prediction on input
-x = torch.rand(10)
-pred = model(x)
-print(f"Prediction Successful: {pred}")
+pred = model(
+    (
+        helper.fen2vec(
+            "r3b2k/p5p1/4pq2/1p1p4/2n2P2/P2B4/1P2Q2P/1K1R2R1 w - - 4 26", isWhite=True
+        )
+    ).unsqueeze(0)
+)
+print(f"Prediction: {pred}")
